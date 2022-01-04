f4ce702c541d231bd3900f2326bdf002a301433a
Philip Lalonde <lalondeph@gmail.com>
2022-01-03 22:11:55 -0500

    rarity parser starer file

---
+++ rarity_parser/src/main/java/rarity_parser/rarity_parser/RarityParserApplication.java
@@ -0,0 +1,13 @@
+package rarity_parser.rarity_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RarityParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RarityParserApplication.class, args);
+	}
+
+}
---
+++ rarity_parser/src/test/java/rarity_parser/rarity_parser/RarityParserApplicationTests.java
@@ -0,0 +1,13 @@
+package rarity_parser.rarity_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RarityParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
